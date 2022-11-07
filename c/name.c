93445cdb6521f4c0d95d8999169f33114205e9d0
2026aleman.marlen@fcboe.org <2026aleman.marlen@fcboe.org>
2022-11-07 14:45:40 -0500

    Added input programs

---
+++ c/name.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+ int main() { 
+	 char name[30]; 
+	 printf("enter the name of your charachter: "); 
+	 scanf("%s", &name) ; 
+	 printf("your charactere's name is %s %s" , name, last);
+	   return 0; 
